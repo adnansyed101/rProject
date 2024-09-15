c72631da77c2b16b0c1de9f61426e32223ab1d42
Adnan <sirgeant10@gmail.com>
2024-09-15 14:20:12 +0600

    Removed Spacing

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -2,27 +2,10 @@ const db = require("../db/queries");
 
 async function getAllTrips(req, res) {
   const trips = await db.getAllTripsFromDb();
-  console.log(trips);
   let total = 0;
-
   for (const trip of trips) {
     total = total + parseInt(trip.price);
-    console.log(trip.tripid);
   }
-
-  res.render("cart", { trips: trips, total: total });
-}
-
-async function deleteTrips(req, res) {
-  const trips = await db.getAllTripsFromDb();
-  console.log(trips);
-  let total = 0;
-
-  for (const trip of trips) {
-    total = total + parseInt(trip.price);
-    console.log(trip.tripid);
-  }
-
   res.render("cart", { trips: trips, total: total });
 }
 
