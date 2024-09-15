8b23232a4a60ac15868600484b6fb6a92afe9c7b
Adnan <sirgeant10@gmail.com>
2024-09-15 16:19:55 +0600

    Changed trips to cart because of rename of table in db

--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,7 @@ const pool = require("./pool");
 
 async function getAllTripsFromDb() {
   const { rows } = await pool.query(
-    "SELECT tripid, tripname,price FROM trips JOIN triptypes ON trips.triptypeid = triptypes.triptypeid"
+    "SELECT cartitemid, tripname, price FROM cart JOIN triptypes ON cart.triptypeid = triptypes.triptypeid"
   );
   return rows;
 }
@@ -13,27 +13,27 @@ async function getAllIndividualTripFromDb() {
 }
 
 async function insertIntoTripsDb(id) {
-  await pool.query("INSERT INTO trips (triptypeid) VALUES ($1)", [id]);
+  await pool.query("INSERT INTO cart (triptypeid) VALUES ($1)", [id]);
 }
 
 async function countCartRowsFromDb() {
-  const { rows } = await pool.query("SELECT COUNT(*) FROM trips");
+  const { rows } = await pool.query("SELECT COUNT(*) FROM cart");
   return rows;
 }
 
 async function deleteItemFromCart(id) {
-  await pool.query("DELETE FROM trips WHERE tripid = " + id);
+  await pool.query("DELETE FROM cart WHERE tripid = " + id);
 }
 
 async function getTotalAmountFromDb() {
   const { rows } = await pool.query(
-    "SELECT SUM(price) FROM trips JOIN triptypes ON trips.triptypeid = triptypes.triptypeid"
+    "SELECT SUM(price) FROM cart JOIN triptypes ON cart.triptypeid = triptypes.triptypeid"
   );
   return rows[0].sum;
 }
 
 async function deleteRowsAndRestart() {
-  await pool.query("TRUNCATE TABLE trips RESTART IDENTITY");
+  await pool.query("TRUNCATE TABLE cart RESTART IDENTITY");
 }
 
 module.exports = {
