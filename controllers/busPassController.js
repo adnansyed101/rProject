906d0c42fe576c6345bd13b8a0199d0b1e59c902
Adnan <sirgeant10@gmail.com>
2024-09-15 14:20:49 +0600

    Added Counting Functionalities to website

--- controllers/busPassController.js
+++ controllers/busPassController.js
@@ -2,13 +2,17 @@ const db = require("../db/queries");
 
 async function getAllIndividualTrip(req, res) {
   const indivualTrip = await db.getAllIndividualTripFromDb();
-  res.render("bus-pass", { indivualTrip: indivualTrip });
+  const countTotalTrips = await db.countCartRowsFromDb();
+
+  res.render("bus-pass", {
+    indivualTrip: indivualTrip,
+    cartTotalTrips: countTotalTrips[0].count,
+  });
 }
 
 async function postInsertCart(req, res) {
   const { id } = req.body;
-
-  db.insertIntoTripsDb(id);
+  await db.insertIntoTripsDb(id);
   res.redirect("/bus-pass");
 }
 
--- db/queries.js
+++ db/queries.js
@@ -4,13 +4,11 @@ async function getAllTripsFromDb() {
   const { rows } = await pool.query(
     "SELECT tripid, tripname,price FROM trips JOIN triptypes ON trips.triptypeid = triptypes.triptypeid"
   );
-
   return rows;
 }
 
 async function getAllIndividualTripFromDb() {
   const { rows } = await pool.query("SELECT * FROM triptypes");
-
   return rows;
 }
 
@@ -18,8 +16,15 @@ async function insertIntoTripsDb(id) {
   await pool.query("INSERT INTO trips (triptypeid) VALUES ($1)", [id]);
 }
 
+async function countCartRowsFromDb() {
+  const { rows } = await pool.query("SELECT COUNT(*) FROM trips");
+  console.log(rows);
+  return rows;
+}
+
 module.exports = {
   getAllTripsFromDb,
   getAllIndividualTripFromDb,
   insertIntoTripsDb,
+  countCartRowsFromDb,
 };
